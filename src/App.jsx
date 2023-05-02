f72f4f9928cdabbd87d328bc6f86b2bf2c7da217
Merajujjaman <asmmerajujjaman@gmail.com>
2023-05-03 01:26:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-400'>welcomr to awesome chef</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </div>
+  );
+};
+
+export default App;
