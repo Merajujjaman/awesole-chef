b0281552146096fff60c048d9005bfd1337c9b0f
Merajujjaman <asmmerajujjaman@gmail.com>
2023-05-03 11:07:52 +0600

    footer done

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navebar from '../Pages/shared/Navebar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/shared/Footer';
 
 const MainLayout = () => {
     return (
         <>
           <Navebar></Navebar>  
           <Outlet></Outlet>
+          <Footer></Footer>
         </>
     );
 };
