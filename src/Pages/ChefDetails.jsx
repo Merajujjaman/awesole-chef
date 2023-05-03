978b89507d9f677e0067eaab2f80354ad476aa40
Merajujjaman <asmmerajujjaman@gmail.com>
2023-05-03 17:18:07 +0600

    recipes detais components created

--- src/Pages/ChefDetails.jsx
+++ src/Pages/ChefDetails.jsx
@@ -4,9 +4,22 @@ import { useLoaderData } from 'react-router-dom';
 const ChefDetails = () => {
     const singleData = useLoaderData()
     console.log(singleData)
+    const { chefPicture, chefName, numberOfRecipes, yearsOfExperience, likes, id, chefBio, recipes,} = singleData
     return (
-        <div>
-            <h1>chef detais is comming soon....</h1>
+        <div className='md:w-4/5 mx-auto my-12'>
+
+            <div className="card lg:card-side bg-base-100 shadow-xl">
+                <div className='md:w-1/2 p-2 '>
+                    <figure><img className='rounded-xl' src={chefPicture} alt="chef picture" /></figure>
+                </div>
+                <div className="card-body md:w-1/2">
+                    <h2 className="card-title">{chefName}</h2>
+                    <p>{chefBio}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Listen</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/RecipeCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecipeCard = () => {
+    return (
+        <div>
+            <h1> this is recipe card component</h1>
+        </div>
+    );
+};
+
+export default RecipeCard;
