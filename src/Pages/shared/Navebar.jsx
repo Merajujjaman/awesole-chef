c06c03828e487bee1eac0c89e6a505313f93f0c3
Merajujjaman <asmmerajujjaman@gmail.com>
2023-05-05 11:53:35 +0600

    fix navigate problem and deploy

--- src/Pages/shared/Navebar.jsx
+++ src/Pages/shared/Navebar.jsx
@@ -6,9 +6,12 @@ import { AuthContext } from '../../Authentication/AuthProvider';
 const Navebar = () => {
     const { user, logOut } = useContext(AuthContext)
 
+
     const handleLogOut = () => {
         logOut()
-            .then(() => { })
+            .then(() => {
+
+            })
             .catch(error => console.log(error))
     }
 
@@ -21,7 +24,7 @@ const Navebar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            
+
                             <li><NavLink
                                 to='/'
                                 className={({ isActive }) => isActive ? 'text-green-500' : ''}
@@ -39,7 +42,7 @@ const Navebar = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        
+
                         <li><NavLink
                             to='/'
                             className={({ isActive }) => isActive ? 'text-green-500' : ''}
